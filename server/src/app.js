2203e38f680f782a62332565a74fec3258b57279
Yevhenii <freshjs.fte@gmail.com>
2021-11-26 21:17:07 +0200

    Separate app from server listen

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./router");
+const handlerError = require("./handlerError/handler");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/public", express.static("public"));
+/* Path: / */
+app.use(router);
+app.use(handlerError);
+
+module.exports = app;
