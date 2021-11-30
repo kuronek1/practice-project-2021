0dd00afacbf2c76efcb45536c22c4f9287021155
Yevhenii <freshjs.fte@gmail.com>
2021-11-30 18:04:27 +0200

    Fix container name

--- sequelize-post-build.sh
+++ sequelize-post-build.sh
@@ -1,9 +1,9 @@
 # Run Sequalize's migrations.
 echo "-----> Running application migrations"
-docker exec -it new-squadhelp_server-dev_1 sequelize db:migrate
+docker exec -it practice-project-2021_server-dev_1 sequelize db:migrate
 echo "<----- Migrated"
 
 # Run Sequalize's seeds.
 echo "-----> Running application seeds"
-docker exec -it new-squadhelp_server-dev_1 sequelize db:seed:all
+docker exec -it practice-project-2021_server-dev_1 sequelize db:seed:all
 echo "<----- Seeds created"
