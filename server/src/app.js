08138d16699b20299440b3812e9da363215f2312
Yevhenii <freshjs.fte@gmail.com>
2021-12-01 19:10:59 +0200

    Minor changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const router = require("./router");
-const handlerError = require("./handlerError/handler");
+const express = require('express');
+const cors = require('cors');
+const router = require('./router');
+const handlerError = require('./handlerError/handler');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/public", express.static("public"));
+app.use('/public', express.static('public'));
+
+/* app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+}); */
 /* Path: / */
 app.use(router);
 app.use(handlerError);
